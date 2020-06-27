821d7a92562ba467012f28665e829ac2ffdea7ba
Arvind Kumar <67261952+dse-arvind@users.noreply.github.com>
2020-06-27 12:04:56 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,15 @@
+#Subset the data for fips == “24510” and then aggregate them by summing the Emissions per years
+
+NEIdataBaltimore<-subset(NEIdata, fips == "24510")
+totalEmissionBaltimore <- aggregate(Emissions ~ year, NEIdataBaltimore, sum)
+totalEmissionBaltimore
+
+#Plotting the Total Emissions for baltimore over Time
+
+barplot(
+  (totalEmissionBaltimore$Emissions)/10^6,
+  names.arg=totalEmissionBaltimore$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (10^6 Tons)",
+  main="Total PM2.5 Emissions From All Baltimore City Sources"
+)
