d485851a88ad45ac9330e222e5cdfb3003789964
Arvind Kumar <67261952+dse-arvind@users.noreply.github.com>
2020-06-27 12:04:14 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,14 @@
+#First we aggregate the data in
+
+totalEmission <- aggregate(Emissions ~ year, NEIdata, sum)
+totalEmission
+
+#Plotting the total Emissions over time using a base plotting
+
+barplot(
+  (totalEmission$Emissions)/10^6,
+  names.arg=totalEmission$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (10^6 Tons)",
+  main="Total PM2.5 Emissions From All US Sources"
+)
